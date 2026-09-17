e11b9354642c290d84db36f51fef25e880cc8f10
profLedson <ledson.vanini@escola.pr.gov.br>
2026-09-17 13:28:45 +0000

    refatorar: atualizar páginas HTML e estilos, melhorar estrutura do servidor

--- 3º TRIMESTRE/express-01/servidor02.js
+++ 3º TRIMESTRE/express-01/servidor02.js
@@ -1,14 +1,16 @@
 import express from   'express'
-import path from 'path' 
+import path from 'path'
 
+const dirBase = import.meta.dirname
 const app = express()
 const porta = 3000
+// Usar Middleware(Guardião)
+app.use(express.static(path.join(dirBase, 'publico')))
+
 // Criar as rotas do servidor
 app.get('/', (req, res) => {
-    res.sendFile('/paginas/index.html', {root: import.meta.dirname})
+    res.sendFile('paginas/index.html', {root: dirBase})
 })
-
-
 // Liberar a porta do meu computador
 app.listen(porta, () => { console.log('Servidor está vivo!')} )
 
